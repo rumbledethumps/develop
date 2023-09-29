28dd9690fa9826a56ee80b3e1cc438fb7213a4d7
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-09-29 12:10:22 -0700

    use xram struct setter

--- src/hello.c
+++ src/hello.c
@@ -4,42 +4,28 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdarg.h>
-
+#include <stddef.h>
 #include <fcntl.h>
-
 
 void main()
 {
-    int i, fd;
-    typedef struct
-    {
-        bool x_wrap;
-        bool y_wrap;
-        int16_t x_pos_px;
-        int16_t y_pos_px;
-        int16_t width_px;
-        int16_t height_px;
-        uint16_t xram_data_ptr;
-        uint16_t xram_palette_ptr;
-    } mode3_config_t;
-    mode3_config_t config = {};
-    config.x_wrap = false;
-    config.x_pos_px = 0;
-    config.y_pos_px = 0;
-    config.width_px = 320;
-    config.height_px = 180;
-    config.xram_data_ptr = 0x0200;
-    config.xram_palette_ptr = 0x0000;
-    RIA.addr0 = 0xFF00;
-    RIA.step0 = 1;
-    for (i = 0; i < sizeof(mode3_config_t); i++)
-        RIA.rw0 = ((uint8_t *)&config)[i];
+    int i, fd, x, y;
+    uint8_t v;
 
-    i = ria_xreg(1, 0, 0, 2);
+    xram0_struct_set(0xFF00, vga_mode3_config_t, x_wrap, true);
+    xram0_struct_set(0xFF00, vga_mode3_config_t, y_wrap, true);
+    xram0_struct_set(0xFF00, vga_mode3_config_t, x_pos_px, 0);
+    xram0_struct_set(0xFF00, vga_mode3_config_t, y_pos_px, 0);
+    xram0_struct_set(0xFF00, vga_mode3_config_t, width_px, 320);
+    xram0_struct_set(0xFF00, vga_mode3_config_t, height_px, 180);
+    xram0_struct_set(0xFF00, vga_mode3_config_t, xram_data_ptr, 0x0200);
+    xram0_struct_set(0xFF00, vga_mode3_config_t, xram_palette_ptr, 0x0000);
+
+    i = xreg(1, 0, 0, 2);
     printf("canvas=%d\n", i);
-    i = ria_xreg(1, 0, 1, 3, 0xFF00, 2);
+    i = xreg(1, 0, 1, 3, 2, 0xFF00);
     printf("mode_3=%d\n", i);
-    i = ria_xreg(1, 0, 1, 0, 2);
+    i = xreg(1, 0, 1, 0, 1);
     printf("mode_0=%d\n", i);
 
     fd = open("kodim23_320.bin", O_RDONLY);
@@ -53,7 +39,17 @@ void main()
     read_xram(0xC000, 0x2300, fd);
     close(fd);
 
+    v = RIA.vsync;
     while (1)
     {
+        if (RIA.vsync == v)
+            continue;
+        xram0_struct_set(0xFF00, vga_mode3_config_t, x_pos_px, x);
+        xram0_struct_set(0xFF00, vga_mode3_config_t, y_pos_px, y);
+        if (++x >= 320)
+            x = -320;
+        if (++y >= 180)
+            y = -180;
+        v = RIA.vsync;
     }
 }
